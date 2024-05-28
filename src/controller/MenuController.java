7626e41511db9f3ef5fa43f187812408eb816c29
Nicolas Ortiz <nicolasortiz2003@gmail.com>
2024-05-28 19:31:47 -0300

    criacao da tela menu com acesso para outras telas

--- src/controller/MenuController.java
+++ src/controller/MenuController.java
@@ -1,4 +1,43 @@
 package controller;
 
+import view.MenuView;
+
 public class MenuController {
+    private MenuView mv;
+    private ListaVaquinhasController lvc;
+    private ListaVaquinhasUsuarioController lvuc;
+    private CriarVaquinhaController cvc;
+    private LoginController lc;
+    private CadastroController cc;
+    private int opcao;
+    public MenuController(){
+        mv = new MenuView();
+        this.opcao = 0;
+        while(this.opcao != 9){
+            this.opcao = mv.menu();
+            if(this.opcao == 1){
+                lc = new LoginController();
+            }else if(this.opcao == 2){
+                cc = new CadastroController();
+            }else{
+                mv.opcaoInvalida();
+            }
+        }
+        while(this.opcao != 9) {
+            this.opcao = mv.menuLogado();
+            if (this.opcao == 1) {
+                lvc = new ListaVaquinhasController();
+            } else if (this.opcao == 2) {
+                lvuc = new ListaVaquinhasUsuarioController();
+            } else if (this.opcao == 3) {
+                cvc = new CriarVaquinhaController();
+            } else if (this.opcao == 4) {
+
+            }else {
+                mv.opcaoInvalida();
+            }
+        }
+
+    }
+
 }
--- src/view/MenuView.java
+++ src/view/MenuView.java
@@ -1,4 +1,32 @@
 package view;
 
+import java.util.Scanner;
+
 public class MenuView {
+    private Scanner sc;
+    public MenuView(){
+        this.sc = new Scanner (System.in);
+    }
+
+    public int menu(){
+        this.sc = new Scanner(System.in);
+        System.out.println("\n(1) Login");
+        System.out.println("(2) Cadastro");
+        System.out.println("(9) Sair");
+        System.out.println("Opção: ");
+        return sc.nextInt();
+    }
+    public int menuLogado(){
+        this.sc = new Scanner(System.in);
+        System.out.println("(1) Lista de vaquinhas disponíveis");
+        System.out.println("(2) Minhas vaquinhas");
+        System.out.println("(3) Criar nova vaquinha");
+        System.out.println("(4) Logout");
+        System.out.println("(9) Sair");
+        System.out.println("Opção: ");
+        return sc.nextInt();
+    }
+    public void opcaoInvalida(){
+        System.out.println("Opção inválida");
+    }
 }
